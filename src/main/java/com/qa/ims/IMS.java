49d9c84b685a68e1c0e829709c07432863f989be
HodanB <Hodanbarkhad@outlook.com>
2022-06-07 19:04:25 +0100

    Added sql file, Added inital ERD, Updated IMS java file

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -6,7 +6,9 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.controller.Action;
 import com.qa.ims.controller.CrudController;
 import com.qa.ims.controller.CustomerController;
+import com.qa.ims.controller.ItemController;
 import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.ItemDAO;
 import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.DBUtils;
 import com.qa.ims.utils.Utils;
@@ -16,12 +18,18 @@ public class IMS {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	private final CustomerController customers;
+	private final ItemController items;
+//private final OrderController order;
 	private final Utils utils;
 
 	public IMS() {
 		this.utils = new Utils();
 		final CustomerDAO custDAO = new CustomerDAO();
+		final ItemDAO itemDAO = new ItemDAO();
+//final OrderDAO ordDAO = new OrdDAO();
 		this.customers = new CustomerController(custDAO, utils);
+		this.items = new ItemController(itemDAO, utils);
+//final this.oders = new OrderController(ordDAO, utils);
 	}
 
 	public void imsSystem() {
@@ -49,9 +57,11 @@ public class IMS {
 			case CUSTOMER:
 				active = this.customers;
 				break;
-			case ITEM:
+			case ITEM: 
+				active = this.items;
 				break;
 			case ORDER:
+//				active = this.order;
 				break;
 			case STOP:
 				return;
