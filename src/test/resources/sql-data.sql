fcd038b72daf954b195d857f898fc4c453b9fc50
HodanB <Hodanbarkhad@outlook.com>
2022-06-09 17:27:11 +0100

    Update sql-data.sql

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,4 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (`items_name`, `price`) VALUES ('iPad', '1000');
+INSERT INTO `orders` (`fk_id`) VALUES (1);
+INSERT INTO `orders_items` (`fk_order_id`, `fk_items_id`, `item_quantity`) VALUES ('1', '1','2');
