23393cbff6814c1741fea2d521ac864dfafac012
HodanB <Hodanbarkhad@outlook.com>
2022-06-09 01:05:58 +0100

    Update sql-schema.sql

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -12,26 +12,28 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
-    `items_id` INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    `items_id` INT(11) NOT NULL AUTO_INCREMENT,
     `items_name` VARCHAR(40) NOT NULL,
+
     `price` DEC(10, 2) NOT NULL,
     PRIMARY KEY (`items_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-
+      order_id INT(11) NOT NULL AUTO_INCREMENT,
       fk_id INT NOT NULL,
-      order_id INT NOT NULL, 
       PRIMARY KEY (order_id, fk_id),
       FOREIGN KEY (fk_id) REFERENCES customers(id)
 );
 
    CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
+      order_itemsID INT(11) NOT NULL AUTO_INCREMENT,
       fk_order_id INT NOT NULL,
       fk_items_id INT NOT NULL, 
-      order_itemsID INT NOT NULL, 
-      
-      PRIMARY KEY (fk_order_id, fk_items_id, order_itemsID),
+      item_quantity INT(50) NOT NULL,
+      order_cost DEC(10, 2) NOT NULL,
+	      
+      PRIMARY KEY (order_itemsID),
       FOREIGN KEY (fk_order_id) REFERENCES orders(order_id),
       FOREIGN KEY (fk_items_id) REFERENCES items(items_id)
 );
